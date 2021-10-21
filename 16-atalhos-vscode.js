9b57dbac806dca069429ec4f0d1d0eb49810ba1e
Igor Koury <87314880+igorkoury@users.noreply.github.com>
2021-10-21 12:28:53 -0300

    Add files via upload

---
+++ 16-atalhos-vscode.js
@@ -0,0 +1,39 @@
+/* 
+ATALHOS UTEIS DO VSCODE
+
+crtl + ou crtl -: altera o tamanho do texto
+
+Shif + Alt + F: formata o arquivo e corrige espaçamento e indentação
+
+Alt + Z: quebra de linha
+
+Crtl + .: quick fix (a IDE sugere formas de consertar o código que não está complilando)
+
+Alt + arrowUp ou Alt + arrowDown: move a linha para cima ou para baixo 
+
+F8: leva até o erro
+
+
+========    Alterando nome de variável  ========
+
+Crtl F2: seleciona todas as palavras iguais. ex.: útil para alterar nome de variável em todo o código de uma vez
+
+Crtl B: seleciona palavras iguais (uma a uma) no código para alterar de uma vez
+
+F2: altera o nome da variável selecionada em todo o código
+
+========
+
+F12: mostra a primeira vez em que a variável foi escrita
+
+Crtl + ;: Serve para comentar e descomentar um bloco de código
+
+
+
+========    Extensões interessantes ========
+
+Live Share: permite que mais usuários editem simultaneamente o mesmo arquivo 
+
+========
+
+*/
